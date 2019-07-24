ed6040f49971d40a63d627a60c24cf406f90faa1
Nikolay Ivanov <n_13@bk.ru>
2019-07-25 00:46:31 +0300

    добавил заголовок, добавил анимацию появления

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,18 +2,21 @@
 
 // import jQuery from './vendor/jquery-3.2.1';
 import {tagCloudSettings} from './parts/other/tagcloud-init';
+import AOS from './parts/aos';
 
 // window.jQuery = window.$ = jQuery;
 
 (() => {
-
   // не забыть удалить
   const hello = (input) => {
     console.log(input);
   };
   hello('Page is Ready');
 
-  console.log(tagCloudSettings);
+  // инициализация плагина AOS
+  AOS.init();
+
+  // инициализация облака тегов на главной
   $('.js-tagcloud').svg3DTagCloud(tagCloudSettings.indexTagCloudSettings);
 
 })();
